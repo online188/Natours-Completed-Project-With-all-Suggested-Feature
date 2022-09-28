da4a2cc59a0fb15248994174d870f00b01939156
Hoang Nam <alphalinkmedia@gmail.com>
2022-09-28 21:42:39 +0700

    test

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,7 +52,7 @@ const createBookingCheckout = async session => {
   console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_item_group.subtotal / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
