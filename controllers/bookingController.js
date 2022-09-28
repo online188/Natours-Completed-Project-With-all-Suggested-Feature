86f50d8cd5906cd52b18d0be6652a08d22de424c
Hoang Nam <alphalinkmedia@gmail.com>
2022-09-28 19:54:15 +0700

    test

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -68,7 +68,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'payment_intent.succeeded')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
